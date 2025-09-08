5c2591210a1be180f4ca8c0d8f44b286db710da5
ValerieVea <vianneval514@gmail.com>
2025-09-08 11:21:06 +0700

    feat: implement login register page

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,7 @@
+<div class="bg-white rounded-lg shadow-md p-4 mb-4">
+    <h3 class="font-bold text-lg text-gray-800">{{ $kantin }}</h3>
+    <p class="text-gray-600">{{ $nama }}</p>
+    <p class="text-blue-600 font-semibold">Rp{{ number_format($harga,0,',','.') }}</p>
+    <button class="mt-2 bg-blue-500 text-white px-4 py-2 rounded-lg">Pesan</button>
+    
+</div>
