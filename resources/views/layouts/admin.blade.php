9ead39d3e6d52c6230498f9b7b54b5c01c759aa5
ValerieVea <vianneval514@gmail.com>
2025-11-23 21:47:24 +0700

    vivianne/implement-admin-page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,11 +24,6 @@
             <span class="font-bold text-lg">Laper.in</span>
         </a>
 
-        <a href="#" 
-           class="flex items-center w-full space-x-2  'bg-white/10' : 'hover:bg-white/10' }} rounded-md px-3 py-2 transition">
-            <i class="fa-solid fa-chart-simple text-white"></i>
-            <span>Dashoard</span>
-        </a>
 
         <a  href="{{ route('canteens.index') }}"
            class="flex items-center w-full space-x-2 'bg-white/10' : 'hover:bg-white/10' }} rounded-md px-3 py-2 transition">
